56d022a4f7ba92c63e6753c7f36af41471f0debe
akshayashok12 <108693238+akshayashok12@users.noreply.github.com>
2023-01-28 18:20:18 +0400

    new model

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,35 @@
 /*!
-* Start Bootstrap - Shop Homepage v5.0.4 (https://startbootstrap.com/template/shop-homepage)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
-*/
+ * Start Bootstrap - Shop Homepage v5.0.4 (https://startbootstrap.com/template/shop-homepage)
+ * Copyright 2013-2021 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
+ */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+/*window.onload = (Event)=>{
+    let myAlert=document.querySelector('.toast');
+    let bsAlert = new bootstrap.Toast(myAlert);
+    bsAlert.show();
+
+}*/
+
+$(document).ready(function () {
+  $("#btnUparrow").click(function () {
+    $(".toast").toast("show");
+  });
+});
+$(document).ready(function () {
+  $("#btnDownarrow").click(function () {
+    $(".toast").toast("show");
+  });
+});
+$(document).ready(function () {
+  $("#btnDoubleup").click(function () {
+    $(".toast").toast("show");
+  });
+});
+$(document).ready(function () {
+  $("#btnDoubledown").click(function () {
+    $(".toast").toast("show");
+  });
+});
+
